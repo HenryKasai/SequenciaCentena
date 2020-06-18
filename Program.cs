a631e982f2f09f89f47a6a9b9109448f6cbd4bfe
HenryKasai <henrykasai1775@gmail.com>
2020-06-18 18:08:02 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("A seguir, os 100 primeiro números não nulos: ");
+            int um = 1;
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            while(um <= 100)
+            {
+                Console.Write($"{um } ");
+                um ++;
+            }
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
